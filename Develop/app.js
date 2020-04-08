6047aef3bafbc62bca6f77dfb4a1b2af37d57fce
Misha Gringauze <mgringauze@gmail.com>
2020-04-07 22:29:32 -0700

    fixing node modules

--- Develop/app.js
+++ Develop/app.js
@@ -30,6 +30,6 @@ const render = require("./lib/htmlRenderer");
 ​
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
+// for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
